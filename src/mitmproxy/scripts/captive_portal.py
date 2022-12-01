cbd6ef9fd032cf5b8a80c14829f4811c64ce70d3
IanDesuyo <59338745+IanDesuyo@users.noreply.github.com>
2022-12-01 21:55:30 +0800

    add captive portal

---
+++ src/mitmproxy/scripts/captive_portal.py
@@ -0,0 +1,47 @@
+import os
+from mitmproxy.http import HTTPFlow, Response
+from mitmproxy.addons.asgiapp import WSGIApp, serve
+from flask import Flask, request, render_template
+from pathlib import Path
+
+file_dir = Path(__file__).absolute().parent / "portal"
+
+
+class CaptivePortal(WSGIApp):
+    def __init__(self):
+        self.portal_domain = os.getenv("PORTAL_DOMAIN", "example.com")
+        self.allow_clients = []
+        
+        self.app = Flask("CaptivePortal", template_folder=file_dir)
+        self.app.add_url_rule("/", "index", self.index, methods=["GET"])
+        self.app.add_url_rule("/", "allow", self.allow, methods=["POST"])
+
+        super().__init__(self.app, self.portal_domain, 80)
+
+    async def request(self, flow: HTTPFlow) -> None:
+        print(flow.request.pretty_host)
+        if self.should_serve(flow):
+            return await serve(self.asgi_app, flow)
+
+        client_ip = flow.client_conn.peername[0]
+        if client_ip not in self.allow_clients:
+            # redirect to portal
+            flow.response = Response.make(
+                302,
+                headers={
+                    "Location": f"http://{self.portal_domain}",
+                    "Cache-Control": "no-cache",
+                },
+            )
+
+    def index(self):
+        return render_template("index.html")
+
+    def allow(self):
+        client_ip = request.remote_addr
+        self.allow_clients.append(client_ip)
+
+        return render_template("success.html", client_ip=client_ip)
+
+
+addons = [CaptivePortal()]
--- src/mitmproxy/scripts/inject.py
+++ src/mitmproxy/scripts/inject.py
@@ -1,3 +1,4 @@
+import os
 from mitmproxy.http import HTTPFlow
 from mitmproxy import ctx
 from pathlib import Path
@@ -6,8 +7,12 @@ file_dir = Path(__file__).absolute().parent / "inject"
 INJECT_HTTP = (file_dir / "http.html").read_bytes()
 INJECT_HTTPS = (file_dir / "https.html").read_bytes()
 
+portal_domain = os.getenv("PORTAL_DOMAIN", "example.com")
 
 async def response(flow: HTTPFlow) -> None:
+    if flow.request.pretty_host == portal_domain:
+        return
+        
     if flow.response and flow.response.content and "text/html" in flow.response.headers.get("content-type"):
         ctx.log.info(f"MAGIC! {flow.request.pretty_url}")
 
