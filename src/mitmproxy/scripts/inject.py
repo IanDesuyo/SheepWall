05f47960a4dc30b5244e7618fe0201a60d243b62
IanDesuyo <59338745+IanDesuyo@users.noreply.github.com>
2022-12-15 19:21:05 +0800

    fix bugs

--- src/mitmproxy/scripts/inject.py
+++ src/mitmproxy/scripts/inject.py
@@ -9,13 +9,14 @@ INJECT_HTTPS = (file_dir / "https.html").read_bytes()
 
 portal_domain = os.getenv("PORTAL_DOMAIN", "example.com")
 
+
 async def response(flow: HTTPFlow) -> None:
     if flow.request.pretty_host == portal_domain:
         return
-        
+
     if flow.response and flow.response.content and "text/html" in flow.response.headers.get("content-type"):
         ctx.log.info(f"MAGIC! {flow.request.pretty_url}")
 
         flow.response.content = flow.response.content.replace(
-            b"</body>", INJECT_HTTPS if flow.request.scheme == "https" else INJECT_HTTP + b"</head>"
+            b"</body>", (INJECT_HTTPS if flow.request.scheme == "https" else INJECT_HTTP) + b"</body>"
         )
